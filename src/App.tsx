dbb0b124f21083eb82fe6d7a7992daf098b5f55c
Dmitriifd <idimka9@gmail.com>
2023-06-01 18:23:57 +0500

    feat: setting styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <header>
+        <div className="test__container">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+          praesentium quod in ipsam, recusandae minus expedita consectetur
+          ducimus inventore ullam quae, perspiciatis deleniti ab quo dolorum
+          esse tenetur facilis cupiditate.
+        </div>
+      </header>
+      <main>
+        <div className="test__container">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+          praesentium quod in ipsam, recusandae minus expedita consectetur
+          ducimus inventore ullam quae, perspiciatis deleniti ab quo dolorum
+          esse tenetur facilis cupiditate.
+        </div>
+      </main>
+      <footer>
+        <div className="test__container">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+          praesentium quod in ipsam, recusandae minus expedita consectetur
+          ducimus inventore ullam quae, perspiciatis deleniti ab quo dolorum
+          esse tenetur facilis cupiditate.
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
