6dbfb7179942297ef76ebeff57f48ca46d02569d
Dmitriifd <idimka9@gmail.com>
2023-06-03 21:23:53 +0500

    feat: product-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
 import { PopularSlider } from 'components/PopularSlider';
+import { ProductCard } from 'components/ProductCard';
 import { StoriesSlider } from 'components/StoriesSlider';
+
+const tempData = [
+  {
+    id: 1,
+    title: 'Пепперони фреш',
+    description:
+      'Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус',
+    price: '289 ₽',
+    img: 'assets/pizza/1.png'
+  },
+  {
+    id: 2,
+    title: 'Пепперони фреш',
+    description:
+      'Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус',
+    price: '289 ₽',
+    img: 'assets/pizza/1.png'
+  },
+  {
+    id: 3,
+    title: 'Пепперони фреш',
+    description:
+      'Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус',
+    price: '289 ₽',
+    img: 'assets/pizza/1.png'
+  },
+  {
+    id: 4,
+    title: 'Пепперони фреш',
+    description:
+      'Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус',
+    price: '289 ₽',
+    img: 'assets/pizza/1.png'
+  },
+  {
+    id: 5,
+    title: 'Пепперони фреш',
+    description:
+      'Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус',
+    price: '289 ₽',
+    img: 'assets/pizza/1.png'
+  },
+];
 
 function App() {
   return (
@@ -10,6 +54,16 @@ function App() {
       <main className="main">
         <StoriesSlider />
         <PopularSlider />
+        <section className="pizza">
+          <div className="pizza__container">
+            <h2 className="pizza__title title">Пицца</h2>
+            <div className="pizza__wrapper grid-wrapper">
+              {tempData.map((item) => (
+                <ProductCard key={item.id} {...item} />
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </>
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,33 @@
+import './ProductCard.scss';
+
+interface ProductCardProps {
+  img: string;
+  title: string;
+  description: string;
+  price: string;
+  id: number;
+}
+
+const ProductCard = ({ img, title, description, price }: ProductCardProps) => {
+  return (
+    <article className="pizza__item product-card">
+      <a href="#" className="product-card__img">
+        <img src={img} alt={title} />
+      </a>
+      <div className="product-card__body">
+        <h3 className="product-card__title">
+          <a href="#" className="product-card__link">
+          {title}
+          </a>
+        </h3>
+        <p className="product-card__description">{description}</p>
+        <div className="product-card__footer">
+          <p className="product-card__price">{price}</p>
+          <button className="product-card__button">Выбрать</button>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export { ProductCard };
